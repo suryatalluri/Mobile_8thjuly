2520d95a122dc096251c27d2d745b466445396dd
root <root@45a5f753eae5>
2016-07-08 06:03:16 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "mobile",
+  "defaultLanguage" : "en",
+  "displayName" : "Mobile_8thjuly",
+  "homePage" : "Main",
+  "name" : "Mobile_8thjuly",
+  "platformType" : "MOBILE",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
